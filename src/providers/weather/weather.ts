a2df60e91d54349306db9daba6385d15d6dc641f
jrcaranddang <jrcaranddang@gmail.com>
2017-11-13 01:58:24 +0000

    weatherProvider: conditionally applied proxy if desktop

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,41 +1,45 @@
 import { Injectable } from '@angular/core';
 // import { Http } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
+import { Platform } from 'ionic-angular';
 import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class WeatherProvider {
   apiKey = '99dfe35fcb7de1ee';
-  url;
+  desktop: Boolean = false;
 
   constructor(
-    public http: HttpClient) {
+    public http: HttpClient,
+    public plt: Platform) {
     // public http: HttpClientModule) {
     console.log('Hello WeatherProvider Provider');
-    this.url = '/api/'+this.apiKey+'/conditions/q';
-    // this.url = 'https://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+    if (this.plt.is('core')) {
+      // This will only print when on desktop
+      this.desktop = true;
+      console.log('I am a desktop device!');
+    }
   }
 
   getWeather(city, state){
     let url = '/api/'+this.apiKey+'/conditions/q/';
-    return this.http.get(url+state+'/'+city+'.json');
-      // .map(res => res.json());
+    return this.desktop ? this.http.get(url+state+'/'+city+'.json') : this.http.get('https://api.wunderground.com/api/'+this.apiKey+'/conditions/q'+state+'/'+city+'.json');
   }
 
   getWeatherCoords(lat, lon) {
     let url = '/api/'+this.apiKey+'/conditions/q/';
-    return this.http.get(url+'/'+lat+','+lon+'.json');
+    return this.desktop ? this.http.get(url+'/'+lat+','+lon+'.json') : this.http.get('https://api.wunderground.com/api/'+this.apiKey+'/conditions/q'+'/'+lat+','+lon+'.json');
   }
 
   search(query: string) {
     let searchUrl = "/search/aq?query=";
-    return this.http.get(searchUrl+query);
-      // .map(res => res.json());
+    return this.desktop ? this.http.get(searchUrl+query) : this.http.get('http://autocomplete.wunderground.com/aq?query='+query);
   }
   
   geoLookupCoords(lat, lon) {
     // http://api.wunderground.com/api/c56568eedbc03ac8/conditions/q/37.776289,-122.395234.json
     let geoUrl = /api/+this.apiKey+'/geolookup/q/';
-    return this.http.get(geoUrl+lat+','+lon+'.json');
+    return this.desktop ? this.http.get(geoUrl+lat+','+lon+'.json') : this.http.get('http://api.wunderground.com/api/'+this.apiKey+'/geolookup/q/'+lat+','+lon+'.json');
   }
 }
